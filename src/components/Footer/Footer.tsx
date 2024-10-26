e98812dec08e5f71c8406e44c3e2e1999afdec3c
PabloRius <pablogrius@gmail.com>
2024-10-26 22:23:41 +0100

    Refactor Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,48 @@
-import "./Footer.css";
-import { NavigationLinkList } from "./Navigation/NavigatonLinkList/NavigationLinkList";
-import { SocialList } from "./SocialList/SocialList";
+import { IconButton } from "../IconButton";
+
+import { DefaultNavigationLinkListDefinition } from "./NavigationLinkListDefinition";
+
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export function Footer() {
+  const links = DefaultNavigationLinkListDefinition;
   return (
     <footer>
-      <SocialList></SocialList>
-      <NavigationLinkList list="default" />
+      <ul className="SocialList">
+        <li>
+          <IconButton Icon={FaXTwitter} callback={() => {}} />
+        </li>
+        <li>
+          <IconButton Icon={FaInstagram} callback={() => {}} />
+        </li>
+        <li>
+          <IconButton Icon={FaYoutube} callback={() => {}} />
+        </li>
+        <li>
+          <IconButton Icon={FaGithub} callback={() => {}} />
+        </li>
+      </ul>
+      <section className="NavigationLinkList">
+        {links?.map((link, idx_1) => {
+          return (
+            <menu className="Column" key={idx_1}>
+              <h6>{link.title}</h6>
+              <ul className="Body">
+                {link.subtitles.map((sublink, idx_2) => {
+                  return (
+                    <a className="NavigationLink" key={`${idx_1}_${idx_2}`}>
+                      {sublink.subtitle}{" "}
+                    </a>
+                  );
+                })}
+              </ul>
+            </menu>
+          );
+        })}
+      </section>
     </footer>
   );
 }
--- src/components/Footer/Navigation/NavigationLink/NavigationLink.tsx
+++
@@ -1,9 +0,0 @@
-import "./NavigationLink.css";
-
-interface NavigationLinkProps {
-  title: string;
-}
-
-export function NavigationLink({ title }: NavigationLinkProps) {
-  return <button className="NavigationLink">{title}</button>;
-}
