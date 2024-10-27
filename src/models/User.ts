9ffd356ddf18dce23daa205c3ecec896794fafe1
PabloRius <pablogrius@gmail.com>
2024-10-27 13:07:19 +0000

    Update User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,8 +5,10 @@ export interface User {
   email: string;
   password: string;
   hubs: Array<Hub>;
+  profilePic: string;
 }
 export interface registerPayload
   extends Pick<User, "password" | "email" | "username"> {}
 export interface loginPayload extends Pick<User, "password" | "email"> {}
-export interface loggedUser extends Pick<User, "username" | "email"> {}
+export interface loggedUser
+  extends Pick<User, "username" | "email" | "hubs" | "profilePic"> {}
