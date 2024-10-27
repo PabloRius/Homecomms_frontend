ba5602c62bd90afc2bbc36c1acc92e94ec3808ef
PabloRius <pablogrius@gmail.com>
2024-10-27 12:46:49 +0000

    Update User model

--- src/config/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://jhjxmibtxmgsgbvzwebe.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoanhtaWJ0eG1nc2didnp3ZWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NzUzNzQsImV4cCI6MjA0MzU1MTM3NH0.MzbiRjcdSRTQFFzG-oXUK0FPk4n_nSnonN4IGOT5MC8"
-);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,12 @@
+import { Hub } from "./Hub";
+
 export interface User {
   username: string;
   email: string;
   password: string;
+  hubs: Array<Hub>;
 }
-export interface registerPayload extends User {}
+export interface registerPayload
+  extends Pick<User, "password" | "email" | "username"> {}
 export interface loginPayload extends Pick<User, "password" | "email"> {}
 export interface loggedUser extends Pick<User, "username" | "email"> {}
