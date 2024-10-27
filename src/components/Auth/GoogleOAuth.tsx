6fd4643471d685b426d5ac7cf32d3da991aa89ca
PabloRius <pablogrius@gmail.com>
2024-10-27 10:04:03 +0000

    Add Google image

--- src/components/Auth/GoogleOAuth.tsx
+++ src/components/Auth/GoogleOAuth.tsx
@@ -1,3 +1,10 @@
-export function OAuth() {
-  return <button className="GoogleOAuth"></button>;
+import GoogleIcon from "../../images/GoogleIcon.png";
+
+export function GoogleOAuth() {
+  return (
+    <button className="GoogleOAuth">
+      <img src={GoogleIcon} alt="Google Icon" />
+      Continue with Google
+    </button>
+  );
 }
